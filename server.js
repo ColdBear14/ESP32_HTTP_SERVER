6fccd6db062d15f842ba9361fbc55e20c904dbf0
LAPTOP-L4QLQTHA\Hy <ngxuanhy1405@gmail.com>
2025-10-29 20:16:18 +0700

    finish

--- server.js
+++ server.js
@@ -12,41 +12,57 @@ app.use(cors({
   origin: 'http://192.168.4.1:8080' // allow your dashboard origin
 }));
 
-mongoose.connect('mongodb://localhost:27017/va_database', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
+mongoose.connect('mongodb://127.0.0.1:27017/ESP32')  
+    .then(() => console.log('Connected to MongoDB'))
+    .catch(err => console.error('MongoDB connection error:', err));
 
-const ActionHistorySchema = new mongoose.Schema({
+const ActionSchema = new mongoose.Schema({
   name: String,
   action: String,
   timestamp: { type: Date, default: Date.now }
 });
 
-const EnvironmentSensorSchema = new mongoose.Schema({
+const SensorSchema = new mongoose.Schema({
   name: String,
   value: Number,
   timestamp: { type: Date, default: Date.now }
 });
 
-const ActionHistory = mongoose.model('ActionHistory', ActionHistorySchema, 'action_history');
-const EnvironmentSensor = mongoose.model('EnvironmentSensor', EnvironmentSensorSchema, 'environment_sensor');
+const SettingsSchema = new mongoose.Schema({
+  id : Number,
+  name: String,
+  period: Number,
+  timestamp: { type: Date, default: Date.now }
+});
+
+const ActionHistory = mongoose.model('Action', ActionSchema, 'action');
+const EnvironmentSensor = mongoose.model('Sensor', SensorSchema, 'sensor');
+const Settings = mongoose.model('Settings', SettingsSchema, 'settings')
 
 app.post('/addData', async (req, res) => {
+  console.log('Nội dung request:', req.body);
+
   const { collection, ...data } = req.body;
   try {
-    if (collection === 'action_history') {
+    if (collection === 'action') {
       const doc = new ActionHistory(data);
       await doc.save();
       console.log(`Action history saved: ${JSON.stringify(data)}`);
       res.status(200).json({ message: 'Action history saved' });
-
-    } else if (collection === 'environment_sensor') {
+    } 
+    else if (collection === 'sensor') {
       const doc = new EnvironmentSensor(data);
       await doc.save();
       console.log(`Environment sensor saved: ${JSON.stringify(data)}`);
       res.status(200).json({ message: 'Environment sensor saved' });
-    } else {
+    } 
+    else if (collection === 'settings'){
+      const doc = new Settings(data);
+      await doc.save();
+      console.log(`Settings saved: ${JSON.stringify(data)}`);
+      res.status(200).json({ message: 'Settings saved' });
+    } 
+    else {
       res.status(400).json({ error: 'Invalid collection' });
     }
   } catch (err) {
@@ -62,10 +78,10 @@ app.get('/getData10', async (req, res) => {
       query.name = name;
     }
 
-    if (collection === 'action_history') {
+    if (collection === 'action') {
       const data = await ActionHistory.find(query).sort({ timestamp: -1 }).limit(10);
       res.status(200).json(data);
-    } else if (collection === 'environment_sensor') {
+    } else if (collection === 'sensor') {
       const data = await EnvironmentSensor.find(query).sort({ timestamp: -1 }).limit(10).select("name value timestamp");
       console.log("Enviroment sensor send data successfual");
       res.status(200).json(data);
@@ -85,10 +101,10 @@ app.get('/getData', async (req, res) => {
       query.name = name;
     }
 
-    if (collection === 'action_history') {
+    if (collection === 'action') {
       const data = await ActionHistory.find(query).sort({ timestamp: -1 }).limit(1).select("name action timestamp");
       res.status(200).json(data);
-    } else if (collection === 'environment_sensor') {
+    } else if (collection === 'sensor') {
       const data = await EnvironmentSensor.find(query).sort({ timestamp: -1 }).limit(1).select("name value timestamp");
       console.log("Enviroment sensor send data successfual");
       res.status(200).json(data);
@@ -102,14 +118,15 @@ app.get('/getData', async (req, res) => {
 
 app.listen(3000, () => {
   const interfaces = os.networkInterfaces();
-  let address = 'localhost';
+  console.log('Tất cả địa chỉ mạng:');
+  
   for (const name of Object.keys(interfaces)) {
     for (const iface of interfaces[name]) {
-      if (iface.family === 'IPv4' && !iface.internal) {
-        address = iface.address;
-        break;
+      if (iface.family === 'IPv4') {
+        console.log(`Interface: ${name}, IP: ${iface.address}, Internal: ${iface.internal}`);
       }
     }
   }
-  console.log(`Server started on http://${address}:3000`);
+  
+  console.log(`Server started on port 3000`);
 });
